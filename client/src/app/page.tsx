f562a16def3f50037863d3e092825cf8484bbce7
Jackie Lin <JLin226859643@gmail.com>
2025-09-12 15:43:00 -0400

    setup

---
+++ client/src/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-blue-600">
+      <h1 className="text-6xl text-red-500">
+        Tailwind is working! 🎉
+      </h1>
+    </div>
+  )
+}
+
+
+
